020d6a680d9ed316a2c1769441857f0c6c03fe2e
Yeong joon <bluehome8626@naver.com>
2023-07-05 02:52:42 +0900

    feat : 이력서 작성 페이지

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import UserSign from "./components/sign/user_sign";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Start from "./components/startt";
 import Login from "./components/login";
-import MenuPage from "./pages/menu";
 import Tab from "./components/common/tab";
+import WritePage from "../src/pages/WritePage";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
           <Route path="/" element={<Start />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/sign" element={<UserSign />}></Route>
-          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/write" element={<WritePage />} />
         </Routes>
       </BrowserRouter>
     </div>
