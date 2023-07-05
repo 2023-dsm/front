f9c7eefcc9a6ff76ff9ef79438d5f4365973387f
hyuna <hyuna_0525@dsm.hs.kr>
2023-07-05 14:53:22 +0900

    head user info update

--- src/components/head/head.jsx
+++ src/components/head/head.jsx
@@ -1,11 +1,28 @@
 import "./head.css";
 import gosu from "../../../src/assets/고수.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Head() {
+  const [name, setName] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`http://192.168.1.149:8080/user/info`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      })
+      .then((res) => {
+        setName(res.data.name);
+        console.log(res);
+      });
+  }, []);
+
   return (
     <div className="head">
       <img src={gosu} width="30px" />
-      <span>김승진님 안녕하세요</span>
+      <span>{`${name}`}님 안녕하세요</span>
     </div>
   );
 }
