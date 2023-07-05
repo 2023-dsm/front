88012d733f1b761c88f2f2733070de865e63db08
hyuna <hyuna_0525@dsm.hs.kr>
2023-07-05 11:34:04 +0900

    설명 수정

--- src/nextpages.jsx
+++ src/nextpages.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import PageOne from "./assets/A.png";
 import PageTwo from "./assets/B.png";
 import PageThree from "./assets/C.png";
-import PageFore from "./assets/D.png";
+import PageFour from "./assets/D.png";
 import PageFive from "./assets/E.png";
 import Close from "./assets/close.png";
 import "./Close.css";
+import main from "./components/startt/index";
 
 const ImageSlider = () => {
+  const navigate = useNavigate();
+
   const settings = {
     arrows: true,
     dots: false,
@@ -18,27 +22,45 @@ const ImageSlider = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-
     autoplay: false,
+  };
+
+  const close = () => {
+    navigate("/main");
+    // Define the close function logic here
   };
 
   return (
     <Slider {...settings}>
       <div>
-        <img src={Close} className="Close" />
-        <img src={PageOne} width="360px" height="640px" className="mom" />
+        <div onClick={close}>
+          <img src={Close} className="Close" alt="Close" />
+        </div>{" "}
+        <img src={PageOne} height={600} className="mom" alt="Page One" />
       </div>
       <div>
-        <img src={PageTwo} />
+        <div onClick={close}>
+          <img src={Close} className="Close" alt="Close" />
+        </div>
+        <img src={PageTwo} height={600} className="mom" alt="Page Two" />
       </div>
       <div>
-        <img src={PageThree} />
+        <div onClick={close}>
+          <img src={Close} className="Close" alt="Close" />
+        </div>
+        <img src={PageThree} height={600} className="mom" alt="Page Three" />
       </div>
       <div>
-        <img src={PageFore} />
+        <div onClick={close}>
+          <img src={Close} className="Close" alt="Close" />
+        </div>
+        <img src={PageFour} height={600} className="mom" alt="Page Four" />
       </div>
       <div>
-        <img src={PageFive} />
+        <div onClick={close}>
+          <img src={Close} className="Close" alt="Close" />
+        </div>
+        <img src={PageFive} height={600} className="mom" alt="Page Five" />
       </div>
     </Slider>
   );
